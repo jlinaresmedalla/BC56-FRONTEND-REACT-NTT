46981d9ba1aa4e2ab8b8c00d1d819da093dc1fc7
jlinaresmedalla <jlinaresmedalla@gmail.com>
2024-10-18 17:50:00 -0500

    feat: Desarrollar ui de formulario de envío

--- src/pages/resumen/FormSection/FormSection.tsx
+++ src/pages/resumen/FormSection/FormSection.tsx
@@ -1,3 +1,46 @@
+import { Button, Input, Select } from "@/components/UI";
+import "./FormSection.css";
+
 export const FormSection = () => {
-  return <section>form section</section>;
+  return (
+    <section className="form-section">
+      <form className="shipping-form" onSubmit={() => {}}>
+        <h3>
+          <center>Información de envío</center>
+        </h3>
+        <div className="shipping-form-fields">
+          <span className="shipping-form-field">
+            <label className="title">Nombre</label>
+            <Input placeholder="Ingresa tu nombre" />
+          </span>
+          <span className="shipping-form-field">
+            <label className="title">Apellidos</label>
+            <Input placeholder="Ingresa tus apellidos" />
+          </span>
+          <span className="shipping-form-field">
+            <label className="title">Distrito</label>
+            <Select
+              options={[{ value: "Lima", label: "Lima" }]}
+              valueKey={"value"}
+              labelKey={"label"}
+              staticLabel="Selecciona tu distrito"
+            />
+          </span>
+          <span className="shipping-form-field">
+            <label className="title">Dirección</label>
+            <Input placeholder="Ingresa tu dirección" />
+          </span>
+          <span className="shipping-form-field">
+            <label className="title">Referencia</label>
+            <Input placeholder="Referencia de tu dirección" />
+          </span>
+          <span className="shipping-form-field">
+            <label className="title">Celular</label>
+            <Input placeholder="Ingresa tu número" />
+          </span>
+        </div>
+        <Button type="submit">Comprar</Button>
+      </form>
+    </section>
+  );
 };
--- src/pages/resumen/ResumenPage.tsx
+++ src/pages/resumen/ResumenPage.tsx
@@ -1,10 +1,12 @@
 import { CartSection } from "./CartSection/CartSection";
+import { FormSection } from "./FormSection/FormSection";
 import "./ResumenPage.css";
 
 export const ResumenPage = () => {
   return (
     <main>
       <CartSection />
+      <FormSection />
     </main>
   );
 };
